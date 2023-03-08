bb2f963648359c3d234092171a517a45365ecb61
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-08 15:57:14 -0800

    v4

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    sum = int(a) + int(b)
-    print(int(sum), end='')
-    return int(sum)
+    print("{}".format(int(a) + int(b), end=''))
+    return int(a) + int(b)
