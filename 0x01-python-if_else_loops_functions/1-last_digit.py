d926e1554dd6711f9916e7de15e51ef87eea18c4
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-08 11:40:45 -0800

    fixing PEP issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    remainder = number % -10
+    remainde = number % -10
 else:
-    remainder = number % 10
-if remainder == 0:
-    print(f'Last digit of {number} is {remainder} and is 0')
-elif remainder < 6 and remainder != 0:
-    print(f'Last digit of {number} is {remainder} and is less than 6 and not 0')
-elif remainder > 5:
-    print(f'Last digit of {number} is {remainder} and is greater than 5')
+    remainde = number % 10
+if remainde == 0:
+    print(f'Last digit of {number} is {remainde} and is 0')
+elif remainde < 6 and remainde != 0:
+    print(f'Last digit of {number} is {remainde} and is less than 6 and not 0')
+elif remainde > 5:
+    print(f'Last digit of {number} is {remainde} and is greater than 5')
