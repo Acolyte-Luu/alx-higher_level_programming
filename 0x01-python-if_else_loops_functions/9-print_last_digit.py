813e22638b902d739b3906a4565dbb5676f80e68
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-08 15:14:58 -0800

    bug fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         remainder = number % -10
+        return remainder
     else:
         remainder = number % 10
-    return remainder
+        return remainder
