ef9710e67ac73c05cf59ba9e619a07449f0210f9
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-13 15:53:33 -0700

    adding script line

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for item in my_list:
         if idx < 0:
@@ -7,4 +8,3 @@ def replace_in_list(my_list, idx, element):
         else:
             my_list[idx] = element
             return my_list
-
