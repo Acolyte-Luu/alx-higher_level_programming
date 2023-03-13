42d633efe243dac2b9d7d260044cefaf97a646cc
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-13 15:50:55 -0700

    modifying lists in python

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+def replace_in_list(my_list, idx, element):
+    for item in my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list) - 1:
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
+
