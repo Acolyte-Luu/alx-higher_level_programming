19dc5342def6cfb8f07b142b9d53a979f8e6ff9d
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-03-15 14:28:03 -0700

    fixv3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ != "__main__":
     exit()
+
 argStr = "{:d} argument"
 argc = len(sys.argv) - 1
 if argc == 0:
@@ -11,6 +13,7 @@ elif argc == 1:
 else:
     argStr += 's:'
 print(argStr.format(argc))
+
 i = 0
 for arg in sys.argv:
     if i != 0:
